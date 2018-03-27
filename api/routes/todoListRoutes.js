5973af9dea087544f0c927ee4bfab2be592249ef
facundo rodriguez <facundo.rodriguez@fluxit.com.ar>
2018-03-27 15:03:12 -0300

    se completa la api basica

---
+++ api/routes/todoListRoutes.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = function(app) {
+  var todoList = require('../controllers/todoListController');
+
+  // todoList Routes
+  app.route('/tasks')
+    .get(todoList.list_tasks)
+    .post(todoList.create_task);
+
+
+  app.route('/tasks/:taskId')
+    .get(todoList.read_task)
+    .put(todoList.update_task)
+    .delete(todoList.delete_task);
+};
